dcd1470c6799d55a9b4dca847c76e0ad7c6617dd
MahmedOsama22 <55940200+MahmedOsama22@users.noreply.github.com>
2020-03-31 19:35:48 +0200

    Add files via upload

---
+++ styles/global.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+export const globalStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 30
+  },
+  titleText: {
+    fontFamily: 'nunito-bold',
+    fontSize: 20,
+    color: 'blue'
+  },
+  paragraph: {
+    marginVertical: 8,
+    lineHeight: 20
+  },
+  Button: {
+    marginVertical: 8
+  }
+});
